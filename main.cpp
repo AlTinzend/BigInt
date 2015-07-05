f6e88083daf7604966857908cab0446d8c84de0c
AlTinzend <al.tinzend@gmail.com>
2015-07-05 18:59:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,20 +9,19 @@ int main()
 	while(num1 != "x"){
 		cout<<"Enter two large integers seperated by operand(+,-,*):"<<endl;
 		cin>>num1>>action>>num2;
-			if(action == '+'){
-				cout<<add.sumInt(num1, num2);
-				cout<<endl;
-			}
-			else if(action == '-'){
-				cout<<add.subInt(num1, num2);
-				cout<<endl;
-			}
-			else if(action == '*'){
-				cout<<add.multXInt(num1, num2);
-				cout<<endl;
-			}
-			else{cout<<"Error unknown request.\n";}
-		
+		if(action == '+'){
+			cout<<add.sumInt(num1, num2);
+			cout<<endl;
+		}
+		else if(action == '-'){
+			cout<<add.subInt(num1, num2);
+			cout<<endl;
+		}
+		else if(action == '*'){
+			cout<<add.multXInt(num1, num2);
+			cout<<endl;
+		}
+		else{cout<<"Error unknown request.\n";}
 	}
 	cout<<endl;
 	system ("pause");
